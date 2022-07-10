8ca8bca286fd6394ee15548d5fe349197ccb0446
vaclavsrajer <venda1997@gmail.com>
2022-07-10 18:20:12 +0200

    user auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Notes from "./components/Notes/Notes";
@@ -7,31 +7,58 @@ import SignUp from "./components/SignUp/SignUp";
 import Navigation from "./components/Navigation/Navigation";
 import Popup from "./components/Popup/Popup";
 import { useState } from "react";
+import { auth } from "./firebase/firebase.config";
 
 function App() {
   const [showPopup, setShowPopup] = useState({ showPopup: false, message: "" });
+
+  const PrivateRoute = ({ children }) => {
+    const isAuthenticated = auth;
+    console.log(auth);
   
+    if (isAuthenticated) {
+      return children;
+    }
+  
+    return <Navigate to="/" />;
+  };
+
   return (
     <>
       {true ? (
-        true ? (
+        <div className="container">
+          <PrivateRoute>
+            <Navigation />
+          </PrivateRoute>
           <Routes>
             <Route path="login" element={<Login />} />
             <Route path="sign-up" element={<SignUp />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="contact"
+              element={
+                <PrivateRoute>
+                  <Contact setShowPopup={setShowPopup} />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="notes"
+              element={
+                <PrivateRoute>
+                  <Notes />
+                </PrivateRoute>
+              }
+            />
           </Routes>
-        ) : (
-          <div className="container">
-            <Navigation />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route
-                path="contact"
-                element={<Contact setShowPopup={setShowPopup} />}
-              />
-              <Route path="notes" element={<Notes />} />
-            </Routes>
-          </div>
-        )
+        </div>
       ) : (
         <Popup trigger={showPopup} setTrigger={setShowPopup} />
       )}
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { auth } from "../../firebase/firebase.config";
+import { signOut } from "firebase/auth";
 import Logo from "../../assets/logo.svg";
 
 import "./Navigation.css";
 
 export default function Navigation() {
+
+
+  function logout(){;
+    signOut(auth).then(() => console.log("user has loged out"))
+  }
+
   return (
     <div className="nav-container">
       <div className="nav-logo">
@@ -16,6 +24,9 @@ export default function Navigation() {
         <NavLink to="notes">Notes</NavLink>
         <NavLink to="contact">Contact</NavLink>
       </nav>
+      <button onClick={logout}>Log out</button>
     </div>
   );
 }
+
+
